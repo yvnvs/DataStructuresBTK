b9765e3276d16fa507fc25403484ad1e0e7bd876
Yunus <yunusinne@gmail.com>
2022-11-26 23:17:10 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    class Array<T> : IEnumerable<T> , ICloneable
+    {
+        private T[] InnerList;
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList= new T[2];
+            Count=0;
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
